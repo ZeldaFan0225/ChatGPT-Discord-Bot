89c2fb5da6697da736017eee7424e35f4d1aa14e
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-06-01 09:31:45 +0200

    fix date

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -24,7 +24,7 @@ export async function handleMessage(message: Message, client: ChatGPTBotClient, 
         },
         {
             role: "user",
-            content: `The current date and time is ${new Date().getUTCDate()}. My Discord Username is "${message.member.displayName}". Your knowledge cutoff is 2021.`
+            content: `The current date and time is ${new Date().toUTCString()}. My Discord Username is "${message.member.displayName}". Your knowledge cutoff is 2021.`
         },
         {
             role: "user",
